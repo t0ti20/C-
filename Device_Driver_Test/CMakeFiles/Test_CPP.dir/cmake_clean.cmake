3d14c0589cae1e084b4a90faf03672df7d795983
Khaled El-Sayed <t0ti20@icloud.com>
2024-03-31 12:01:17 +0200

    Upload Files

---
+++ Device_Driver_Test/CMakeFiles/Test_CPP.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Test_CPP.dir/Source/Application.cpp.o"
+  "CMakeFiles/Test_CPP.dir/Source/Application.cpp.o.d"
+  "CMakeFiles/Test_CPP.dir/Source/Driver.cpp.o"
+  "CMakeFiles/Test_CPP.dir/Source/Driver.cpp.o.d"
+  "Test_CPP"
+  "Test_CPP.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Test_CPP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
